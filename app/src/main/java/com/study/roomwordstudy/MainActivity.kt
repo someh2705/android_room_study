2075fa807641bc3b08f26d22fcf5d572bc32c987
someh <someh2705@aiaracorp.com>
2021-11-05 18:39:01 +0900

    feat: word data class and room data table

--- app/src/main/java/com/study/roomwordstudy/MainActivity.kt
+++ app/src/main/java/com/study/roomwordstudy/MainActivity.kt
@@ -2,6 +2,9 @@ package com.study.roomwordstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "word") val word: String)
