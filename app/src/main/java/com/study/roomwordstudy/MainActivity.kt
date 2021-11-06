69c1daa06e6b1814647df9f09293db49476dd5c6
someh <someh2705@gmail.com>
2021-11-06 23:12:07 +0900

    feat: Word recyclerview

--- app/src/main/java/com/study/roomwordstudy/MainActivity.kt
+++ app/src/main/java/com/study/roomwordstudy/MainActivity.kt
@@ -3,8 +3,17 @@ package com.study.roomwordstudy
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.*
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -82,5 +91,44 @@ class WordViewModelFactory(private val repository: WordRepository): ViewModelPro
             return WordViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class WordListAdapter : ListAdapter<Word, WordListAdapter.WordViewHolder>(WordsComparator()) {
+
+    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val wordItemView: TextView = itemView.findViewById(R.id.textView);
+
+        fun bind(text: String?) {
+            wordItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): WordViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recyclerview_item, parent, false)
+
+                return WordViewHolder(view)
+            }
+        }
+    }
+
+    class WordsComparator : DiffUtil.ItemCallback<Word>() {
+        override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem.word == newItem.word
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.word)
     }
 }
