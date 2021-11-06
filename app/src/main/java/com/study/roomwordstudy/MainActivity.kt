fb4e428e590d97580448ac4d2642f0ab315fe92e
someh <someh2705@gmail.com>
2021-11-06 23:12:54 +0900

    feat: connect wordListAdapter to recyclerView

--- app/src/main/java/com/study/roomwordstudy/MainActivity.kt
+++ app/src/main/java/com/study/roomwordstudy/MainActivity.kt
@@ -23,6 +23,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
 
